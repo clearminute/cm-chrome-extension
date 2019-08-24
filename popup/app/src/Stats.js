6b3cc4df41a8e273690f8f3b2327a785043c09cc
Martin Mihaylov <martomi@users.noreply.github.com>
2019-08-24 21:23:26 +0200

    Remove twitter button.
    This is not fully implemented but also the current implementation includes scripts from twitter which is privacy risk.
    
    Signed-off-by: Martin Mihaylov <martomi@users.noreply.github.com>

--- popup/app/src/Stats.js
+++ popup/app/src/Stats.js
@@ -33,26 +33,6 @@ export default function Stats() {
     );
   }, []);
 
-  useEffect(() => {
-    window.twttr = (function(d, s, id) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0],
-        t = window.twttr || {};
-      if (d.getElementById(id)) return t;
-      js = d.createElement(s);
-      js.id = id;
-      js.src = 'https://platform.twitter.com/widgets.js';
-      fjs.parentNode.insertBefore(js, fjs);
-
-      t._e = [];
-      t.ready = function(f) {
-        t._e.push(f);
-      };
-
-      return t;
-    })(document, 'script', 'twitter-wjs');
-  }, []);
-
   console.log(topActivities);
   return (
     <div className={styles.container}>
@@ -69,13 +49,6 @@ export default function Stats() {
           </li>
         ))}
       </ul>
-
-      <a
-        className="twitter-share-button"
-        href="https://twitter.com/intent/tweet?text=Hello%20world"
-      >
-        Tweet
-      </a>
       <a
         className={styles.link}
         href={isProduction ? 'https://light.clearminute.com' : 'http://localhost:8080/'}
