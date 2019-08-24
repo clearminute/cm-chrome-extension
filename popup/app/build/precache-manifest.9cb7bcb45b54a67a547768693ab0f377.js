11dc43c9c63b48b436b5cccc1f58c55dccbad7f0
Martin Mihaylov <martomi@users.noreply.github.com>
2019-08-24 22:53:45 +0200

    Build v0.0.25
    We'll keep the build folder in the repository so that we always know which build is currently deployed in production.
    But also so that we can directly use the following snippet for preparing the zip package for publishing:
    
    git archive --format zip --output clearminute.zip HEAD
    
    Signed-off-by: Martin Mihaylov <martomi@users.noreply.github.com>

---
+++ popup/app/build/precache-manifest.9cb7bcb45b54a67a547768693ab0f377.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beec6e06ce24f72a07b75b08cb3e7795",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a2d3cd9c3e3350bc9086",
+    "url": "./static/css/main.28a37ded.chunk.css"
+  },
+  {
+    "revision": "e0b4411673346cf5381a",
+    "url": "./static/js/2.6d110731.chunk.js"
+  },
+  {
+    "revision": "a2d3cd9c3e3350bc9086",
+    "url": "./static/js/main.2c230ab3.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "083d77cc35c93438ebd51829f3a8e7f7",
+    "url": "./static/media/logo.083d77cc.svg"
+  }
+]);
